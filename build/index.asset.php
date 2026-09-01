c8d4a04a6ecce83ea3eff46f15ebb9b4dd7c71f9
Casper Holten <casperholten@protonmail.com>
2026-09-01 16:21:18 +0200

    Reset form when saving

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-primitives'), 'version' => '43116452dbb7b9a1e6f7');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-primitives'), 'version' => 'f0f9eb2e8b885db0c46e');
