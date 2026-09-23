132b8d99be883f4ac42dcc588be5c9dd790d76b4
Casper Holten <casperholten@protonmail.com>
2026-09-23 21:46:43 +0200

    Add new copyFrom role endpoint

--- includes/REST/RoleController.php
+++ includes/REST/RoleController.php
@@ -38,6 +38,14 @@ class RoleController extends  \WP_REST_Controller
                 'permission_callback' => [ $this, 'permissions_check' ],
             ],
         ]);
+
+        register_rest_route( $this->namespace, '/' . $this->resource_name . '/(?P<slug>[\w-]+)' . '/copyFrom', [
+            [
+                'methods'             => \WP_REST_Server::CREATABLE,
+                'callback'            => [ $this, 'copy_rule' ],
+                'permission_callback' => [ $this, 'permissions_check' ],
+            ],
+        ]);
     }
 
     public function permissions_check( $request ): \WP_Error|bool
@@ -71,6 +79,34 @@ class RoleController extends  \WP_REST_Controller
         $rule->rule_active = true;
         $this->ruleService->save_role_rules($rule);
 
-        return new \WP_REST_Response($rule, 204);
+        return new \WP_REST_Response($rule, 200);
+    }
+
+    public function copy_rule( \WP_REST_Request $request ) {
+      $slug = $request->get_param("slug");
+      $request_json = $request->get_json_params();
+      $copy_from_slug = $request_json['copy_rule_slug'] ?? null;
+
+      if(!$copy_from_slug || $slug == $copy_from_slug) {
+        return new \WP_REST_Response("No rule_id found in request body", 400);
+      }
+
+      $rule = $this->ruleService->get_rule_by_user_role($slug);
+
+      if(!$rule) {
+        $rule = $this->ruleService->add_rule($slug);
+      }
+
+      $copy_from_rule = $this->ruleService->get_rule_by_user_role($copy_from_slug);
+
+      if(!$copy_from_rule) {
+        return new \WP_REST_Response(null, 404);
+      }
+
+      $rule->copy_from_rule($copy_from_rule);
+      
+      $this->ruleService->save_role_rules($rule);
+
+      return new \WP_REST_Response(null, 204);
     }
 }
